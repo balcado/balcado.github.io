91657324d45004d809d9038b030f9b54b0b7b29b
7TE <140772499+7TE2@users.noreply.github.com>
2024-04-23 08:20:23 -0400

    wjhwdwd

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,8 +1,9 @@
 /* Listenner and Transparency Applicator */
 $(window).scroll(function () {
-if ($(window).scrollTop() >= 50)  {
-$('nav').css('background','#10181e');
-} else {
-$('nav').css('background','transparent');
-}
-});
+    if ($(window).scrollTop() >= 50)  {
+    $('nav').css('background','#10181e');
+    } else {
+    $('nav').css('background','transparent');
+    }
+    });
+    
