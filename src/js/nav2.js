27336f879468f208374d8e93f0c69f827ac8808a
7TE <140772499+7TE2@users.noreply.github.com>
2024-04-23 10:08:53 -0400

    wdhbjnkadhwd

--- src/js/nav2.js
+++ src/js/nav2.js
@@ -1,27 +1,25 @@
+$(window).scroll(function () {
+  var scrollTop = $(window).scrollTop();
 
-  $(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-
-    if (scrollTop >= 500 && scrollTop < 3000) {
-      var blurValue = Math.min(8 + (scrollTop - 500) / 100, 20); // Adjust the blur value from 8px to 50px
-      $('nav').css({
-        'background': '#10181e',
-        '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
-        'backdrop-filter': 'blur(' + blurValue + 'px)'
-      });
-    } else if (scrollTop >= 3000) {
-      var blurValue = Math.max(20 - (scrollTop - 3000) / 100, 8); // Adjust the blur value from 50px to 8px
-      $('nav').css({
-        'background': 'transparent',
-        '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
-        'backdrop-filter': 'blur(' + blurValue + 'px)'
-      });
-    } else {
-      $('nav').css({
-        'background': 'transparent',
-        '-webkit-backdrop-filter': 'blur(8px)',
-        'backdrop-filter': 'blur(8px)'
-      });
-    }
-  });
-
+  if (scrollTop >= 500 && scrollTop < 3500) {
+    var blurValue = Math.min(8 + (scrollTop - 500) / 100, 20); // Adjust the blur value from 8px to 20px
+    $('nav').css({
+      'background': '#10181e',
+      '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
+      'backdrop-filter': 'blur(' + blurValue + 'px)'
+    });
+  } else if (scrollTop >= 3500) {
+    var blurValue = Math.max(20 - (scrollTop - 3500) / 100, 8); // Adjust the blur value from 20px to 8px
+    $('nav').css({
+      'background': 'transparent',
+      '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
+      'backdrop-filter': 'blur(' + blurValue + 'px)'
+    });
+  } else {
+    $('nav').css({
+      'background': 'transparent',
+      '-webkit-backdrop-filter': 'blur(8px)',
+      'backdrop-filter': 'blur(8px)'
+    });
+  }
+});
