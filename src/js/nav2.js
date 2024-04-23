c9a7d53ac72c618b03bd8b59cc0d39136d66e11c
7TE <140772499+7TE2@users.noreply.github.com>
2024-04-23 09:42:40 -0400

    wdwdjnkawd

---
+++ src/js/nav2.js
@@ -0,0 +1,27 @@
+
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop >= 500 && scrollTop < 3000) {
+      var blurValue = Math.min(8 + (scrollTop - 500) / 100, 20; // Adjust the blur value from 8px to 50px
+      $('nav').css({
+        'background': '#10181e',
+        '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
+        'backdrop-filter': 'blur(' + blurValue + 'px)'
+      });
+    } else if (scrollTop >= 3000) {
+      var blurValue = Math.max(20 - (scrollTop - 3000) / 100, 8); // Adjust the blur value from 50px to 8px
+      $('nav').css({
+        'background': 'transparent',
+        '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
+        'backdrop-filter': 'blur(' + blurValue + 'px)'
+      });
+    } else {
+      $('nav').css({
+        'background': 'transparent',
+        '-webkit-backdrop-filter': 'blur(8px)',
+        'backdrop-filter': 'blur(8px)'
+      });
+    }
+  });
+
