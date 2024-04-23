01e2adcab9b673567e88ba2678f159f597264098
7TE <140772499+7TE2@users.noreply.github.com>
2024-04-23 09:31:20 -0400

    dwdjknwdjwd

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,14 +3,14 @@
     var scrollTop = $(window).scrollTop();
 
     if (scrollTop >= 500 && scrollTop < 3000) {
-      var blurValue = Math.min(8 + (scrollTop - 500) / 100, 50); // Adjust the blur value from 8px to 50px
+      var blurValue = Math.min(8 + (scrollTop - 500) / 100, 20; // Adjust the blur value from 8px to 50px
       $('nav').css({
         'background': '#10181e',
         '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
         'backdrop-filter': 'blur(' + blurValue + 'px)'
       });
     } else if (scrollTop >= 3000) {
-      var blurValue = Math.max(50 - (scrollTop - 3000) / 100, 8); // Adjust the blur value from 50px to 8px
+      var blurValue = Math.max(20 - (scrollTop - 3000) / 100, 8); // Adjust the blur value from 50px to 8px
       $('nav').css({
         'background': 'transparent',
         '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
