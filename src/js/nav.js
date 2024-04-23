423b801882fb6cc1f18975ec00c9fdf1b65a8d3b
7TE <140772499+7TE2@users.noreply.github.com>
2024-04-23 08:46:03 -0400

    dhgvdwjwddw

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,15 +1,16 @@
-$(window).scroll(function () {
+<script>
+  $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
 
-    if (scrollTop >= 500 && scrollTop < 2500) {
+    if (scrollTop >= 500 && scrollTop < 3000) {
       var blurValue = Math.min(8 + (scrollTop - 500) / 100, 50); // Adjust the blur value from 8px to 50px
       $('nav').css({
         'background': '#10181e',
         '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
         'backdrop-filter': 'blur(' + blurValue + 'px)'
       });
-    } else if (scrollTop >= 2500) {
-      var blurValue = Math.max(50 - (scrollTop - 2500) / 100, 8); // Adjust the blur value from 50px to 8px
+    } else if (scrollTop >= 3000) {
+      var blurValue = Math.max(50 - (scrollTop - 3000) / 100, 8); // Adjust the blur value from 50px to 8px
       $('nav').css({
         'background': 'transparent',
         '-webkit-backdrop-filter': 'blur(' + blurValue + 'px)',
@@ -23,3 +24,4 @@ $(window).scroll(function () {
       });
     }
   });
+</script>
